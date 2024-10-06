e3e355bf135f938d378207f06b27b03ff17f637d
❇️ 𝐒𝐀𝐓𝐇𝐀𝐍𝐈𝐂 ❇️ <uicresher@gmail.com>
2024-10-06 21:01:39 +0530

    𝐒𝐀𝐓𝐇𝐀𝐍𝐈𝐂

--- config.js
+++ config.js
@@ -89,7 +89,7 @@ module.exports = {
   ANTIJID: process.env.ANTIJID === undefined ? "" : process.env.ANTIJID,
   STICKER_PACKNAME:
     process.env.STICKER_PACKNAME === undefined
-      ? "🥰,lyfe00011"
+      ? "sᵃᵗʰᵃⁿⁱᶜ"
       : process.env.STICKER_PACKNAME,
   BRAINSHOP:
     process.env.BRAINSHOP === undefined
